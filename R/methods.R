a7beb76d985688d66b58ba9ac84991f2a101c3fb
WangKoko <yuchi.cc@gmail.com>
2017-07-04 23:31:52 +0800

    [R] sync 20170704 Ting dev result

---
+++ R/methods.R
@@ -0,0 +1,177 @@
+
+library(tidyverse)
+
+GET_LIST <- function(path = 'info/sTW_list.csv')
+{
+  idx <- read_csv(path)[,c(2, 3)]
+  colnames(idx) = c("No", "NAME")
+  idx$No <- str_extract(idx$No, "[0-9]+")
+  return(idx)
+}
+bypass <- c(5258)
+#Strategy
+ANALYSIS_STRATEGY <- function(cata ='TW', id, region)
+{
+  #define
+  cNull <- c(id, NA, NA)
+
+  #ignore id
+  for(i in 1:length(bypass))
+  {
+    if(id == bypass[i])  return(cNull)
+  }
+
+  TEST_REGION <- GET_TEST_REGION(region)
+
+  try.xts(
+    sdata <- GET_DATA(id, cata, year = 3, month = 0),
+    error =  sdata <- NULL
+  )
+  if(is.null(sdata) || length(sdata[,4]) < 60*1.5){
+    return(cNull)
+  }
+
+
+  #1. ma x > ma y
+  try.xts(
+    ma_pos <- na.omit(GET_MA_POS(sdata, tgt = 20, ref = 60)[TEST_REGION]),
+    error =  ma_pos <- NULL
+  )
+  if(is.null(ma_pos) || length(ma_pos) < 1){
+    print("E1: ")
+    return(cNull)
+  }
+
+  #2. close > ma_x
+  try.xts(
+    clma_pos <- na.omit(GET_CLMA_CMP_POS(sdata, tgt = 60)[TEST_REGION]),
+    error = clma_pos <- NULL
+  )
+  if(is.null(clma_pos) || length(clma_pos) < 1){
+    print("E2")
+    return(cNull)
+  }
+
+  #3. b-band
+  bb_pos <- na.omit(GET_BBand_POS(sdata)[TEST_REGION])
+
+  #4. volume - 5 ave. > 20 ave. buy & keep, otherwise, sell
+  vol_pos <- (GET_VOLUME_POS(sdata, ma_vt = 5, ma_vr = 20)[TEST_REGION])
+
+  #Hold
+  hold_pos <- ifelse(ma_pos & bb_pos & vol_pos, 1, 0)
+  hold_time <- GET_HOLD_TIME(hold_pos)
+
+  #profit
+  profit <- ROC(Cl(sdata)) * hold_pos
+  eq <- exp(cumsum(na.omit(profit)))
+  last_eq <- as.numeric(tail(eq, n=1))
+
+  if(hold_time == 0) return (cNull)
+  else
+  {
+    plot(eq)
+    print(c(id, round(last_eq - 1, 2), round((last_eq - 1)/hold_time * 100, 2)))
+    return(c(id, round(last_eq - 1, 2), round((last_eq - 1)/hold_time * 100, 2)))
+  }
+}
+
+GET_VOLUME_POS <-function(sdata, ma_vt = 5, ma_vr = 20)
+{
+  ma_vt_pos <- runMean(sdata[,5], n = ma_vt)
+  ma_vr_pos <- runMean(sdata[,5], n = ma_vr)
+  return(ma_vt_pos > ma_vr_pos)
+}
+
+GET_TEST_REGION <- function(days = 12 * 30)
+{
+  ed_date <- Sys.Date()
+  st_date <- format((ed_date - as.difftime(days, unit = "days")),"%Y-%m-%d")
+  return(paste(st_date, sep="/", ed_date))
+}
+
+GET_DATA <-function(id, cata = 'TW', year = 10, month = 0)
+{
+  target <- paste(id, sep = ".", cata)
+  days <- (year * 12 + month) * 30
+  ed_date <- Sys.time()
+  st_date <- format((ed_date - as.difftime(days, unit = "days")),"%Y-%m-%d")
+
+  try.xts(
+    data <- getSymbols(target, auto.assign = FALSE, from = st_date),
+    error = data <- NULL
+  )
+  if(is.null(data)){
+    print("E1-1: ")
+    return(NULL)
+  }
+  return(na.omit(data))
+}
+
+GET_CLMA_CMP_POS <- function(sdata, tgt = 60)
+{
+  tgt_ma <- runMean(sdata[,4], tgt)
+  return(lag(sdata[,4], k = tgt) >= tgt_ma)
+}
+
+GET_BBand_POS <- function(sdata, n = 22, sd = 1.5, hold = TRUE)
+{
+  hlc <- sdata[, -c(1,5,6)]
+  bb_data <- na.omit(BBands(na.omit(hlc), maType = 'SMA', n, sd))
+
+  buy <- na.omit(Lag(Cl(sdata))>Lag(bb_data$"up"))
+  bb_pos <- buy
+  if(hold){
+    keep <- na.omit(Lag(Cl(sdata))>Lag(bb_data$"mavg"))
+    bb_pos <- GET_BBand_HOLD_POS(buy, keep)
+  }
+  return(bb_pos)
+}
+
+GET_BBand_HOLD_POS <- function(buy, keep)
+{
+  hold = 0;
+  for(i in 1:length(buy))
+  {
+    if(!hold)
+      buy[i] = ifelse(buy[i], 1, 0)
+    else
+      buy[i] = ifelse(keep[i], 1, 0)
+
+    hold = buy[i]
+  }
+  return(buy)
+}
+
+
+GET_MA_POS <- function(sdata, tgt = 20, ref = 60)
+{
+  #ma_5 <- runMean(sdata[,4], n = 5)
+  #ma_10 <- runMean(sdata[,4], n = 10)
+ # ma_20 <- runMean(sdata[,4], n = 20)
+  #ma_60 <- runMean(sdata[,4], n = 60)
+
+
+ # ma_5_10_pos <- ifelse(abs(ma_5 - ma_10)/ma_5 < 0.05, 1, 0)
+ # ma_5_20_pos <- ifelse(abs(ma_20 - ma_5)/ma_5 < 0.05, 1, 0)
+ # ma_5_60_pos <- ifelse(abs(ma_5 - ma_60)/ma_5 < 0.05, 1, 0)
+
+  #ma_pos <- ifelse((ma_5_10_pos & ma_5_20_pos & ma_5_60_pos), 1, 0)
+
+  tgt_ma <- runMean(sdata[,4], n = tgt)
+  ref_ma <- runMean(sdata[,4], n = ref)
+  ma_pos <- tgt_ma > ref_ma
+  return(ma_pos)
+}
+
+GET_MA_Vol_POS <- function(sdata, tgt = 5)
+{
+  tgt_ma <- runMean(sdata[,5], tgt)
+  ma_pos <- lag(sdata[,5], k = tgt) > tgt_ma
+  return(ma_pos)
+}
+
+GET_HOLD_TIME <- function(hold_pos)
+{
+  return(sum(hold_pos))
+}
--- R/r-stock-lib-script.R
+++ R/r-stock-lib-script.R
@@ -1,168 +1,108 @@
+
 library("quantmod")
 library("RCurl")
 library("XML")
 library(stringr)
+library("readr")
 
+# return hold position with array of TRUE FALSE corresponding to date
+bband_strat <- function(stock_name, hist, n = 22, sd = 1.5) {
+  hlc <- hist[, -c(1,5,6)]
+  bb_data <- na.omit(BBands(na.omit(hlc), maType = 'SMA', n, sd))
+  # print(bb_data)
+  # version 4
+  # hold stock if previous day close price > bband up
+  # @324 discuss depends on 323 we buy at 324 then at 324 it fall through up,
+  # then we sell it on 325 OPEN
+  buy <- na.omit(Lag(Cl(hist))>Lag(bb_data$"up"))
+  #print(buy) # debug for holding point
+  pos <- ifelse(buy, 1, 0)
+}
+TW_Reduce <-c(27, 575, 594, 629, 708)
 get_stock_list <- function(cata = 'TW') {
   index_src <- "StockList"
   index_src <- paste(index_src, sep="-", cata)
   index_src <- paste(index_src, sep = "", ".csv")
   src_pth <- paste("info/", sep = "",index_src)
-  idx <- read.csv(src_pth)[,c(1, 2)]
+  idx <- read_csv(src_pth)[,c(1, 2)]
   colnames(idx) = c("No", "Name")
   idx$No <- str_extract(idx$No, "[0-9]+")
-  idx
+  idx#[-TW_Reduce, ]
 }
 
-get_stock_hist <- function(stock_id, month = 8, years = 0) {
-  diff_year = years
+
+
+
+bband_for_single_stock <- function(stock_entry, cata = 'TW', month = 8, years = 0, n = 22, sd = 1.5) {
+  print(stock_entry)
+  stock_id = stock_entry
+  stock_id <- paste(stock_id, sep = ".", cata)
+  diff_year = 0
   diff_month = diff_year*12 + 8
   ed_date = Sys.time()
   # get stock history
   st_date <- format((ed_date - as.difftime(diff_month*30, unit = "days")),"%Y-%m-%d")
-  # print(st_date)
-  # print(ed_date)
-  stock_hist = suppressWarnings(getSymbols(stock_id, auto.assign = FALSE, from = st_date))
-  return(na.omit(stock_hist))
-}
-
-GET_MA_POS <- function(hist, tgt = 60, ref = 120)
-{
-  if (ref > NROW(hist))
-    return(NA)
-
-  tgt_ma <- runMean(Cl(hist), tgt)
-  ref_ma <- runMean(Cl(hist), ref)
-  ma_pos <- tgt_ma > ref_ma
-  return(ma_pos)
-}
-
-# return hold position with array of TRUE FALSE corresponding to date
-pick_strategy <- function(stock_name, cata = 'TW'
-                           , hist = NULL, n = 22, sd = 1.5
-                           , month = 0, years = 1, draw = FALSE, pick = FALSE) {
-  if (is.null(hist)) {
-    stock_name <- paste(stock_name, sep = ".", cata)
-    hist <- get_stock_hist(stock_name, month, years)
-    print(stock_name)
-    # print(hist)
-  }
-
-  hlc <- hist[, -c(1,5,6)]
+  stock_hist = getSymbols(stock_id, auto.assign = FALSE, from = st_date)
+  # print(stock_hist)
+  stock_hist = na.omit(stock_hist)
+  if (nrow(stock_hist)<n)
+    return(c(stock_id, NA, NA))
+  # print(stock_hist)
+  #print(st_date)
+  #print(ed_date)
+  # calculate the performance of bbnad sd = 1.5
+  chartSeries(stock_hist)
+  hlc <- stock_hist[, -c(1,5,6)]
   # print(hlc)
-  if (nrow(hist)<n) {
-    print("> ERROR:: not enough data to calculate SMA")
-    return(NA)
-  }
-  
   bb_data <- na.omit(BBands(na.omit(hlc), maType = 'SMA', n, sd))
-  
-  if (is.null(colnames(bb_data)))
-    return(NA)
-
   # print(bb_data)
-  # hold <- na.omit(Lag(Cl(hist))>Lag(bb_data$"up"))
-  bb_hold <- na.omit(Cl(hist)>bb_data$"up")
-  ma_hold <- GET_MA_POS(hist, tgt = 20, ref = 60)
-
-  if (is.null(colnames(bb_hold))||is.null(ma_hold))
-    return(NA)
-
-  hold <- bb_hold & ma_hold
-  # print(hold) # debug for holding point
-  pos_hold <- ifelse(hold, 1, 0)
-  
-  if(isTRUE(pick)) {
-    # calculate the bband last day (up-dn)/mavg
-    last_bb <- tail(bb_data, n=1)
-    range <- as.numeric((last_bb$'up' - last_bb$'dn')/last_bb$'mavg')
-    # holding position
-    last_2pos <- as.numeric(tail(hold, n=2))
-    # volumn of last day for the stock
-    vol <- as.numeric(tail(Vo(hist), n=1))/1000
-    # calculate the profit
-    buy <- Lag(pos_hold)
-    buy <- ifelse(buy, 1, 0)
-    # dbg usage 
-    if (0) {
-      # print(buy)
-      print("nrow(buy)")
-      print(nrow(buy))
-      print("nrow(hist)")
-      print(nrow(hist))
-    }
-    prof <- OpOp(hist)*buy
-    # print(ret)
-    eq <- exp(cumsum(na.omit(prof)))
-    prof <- tail(eq, n=1) - 1
-    # summarize
-    res <- data.frame(matrix(c(range, last_2pos[1], last_2pos[2], vol, prof),nrow=1,ncol=5))
-    colnames(res) <- c("var","T-1", "T", "Vo", "Prof")
-    rownames(res) <- stock_name
-    # eq3[with(eq3, order(var)), ] # order with column var
-    return(res)
-  } else if (isTRUE(draw)) {
-    chartSeries(hist, name = stock_name, up.col = 'red', dn.col = 'green')
-    plot(addBBands(n = 22, sd = 1.5, maType = "SMA", draw = 'bands', on = 1))
-    if (20 < nrow(hist)) {
-      ma_20<-runMean(hist[,4],n=20)
-      plot(addTA(ma_20,on=1,col="blue"))
-    }
-    if (60 < nrow(hist)) {
-      ma_60<-runMean(hist[,4],n=60)
-      plot(addTA(ma_60,on=1,col="yellow"))
-    }
-    if (120 < nrow(hist)) {
-      ma_120<-runMean(hist[,4],n=120)
-      plot(addTA(ma_120,on=1,col=8))
-    }
-    hold_price <- max(Cl(hist))*Lag(pos_hold)
-    # print(hold_price)
-    plot(addTA(hold_price,on=1,col="grey"))
-  }
-  return(pos_hold)
+  buy <- na.omit(Lag(Cl(stock_hist))>Lag(bb_data$"up"))
+  pos <- ifelse(buy, 1, 0)
+  # print(pos)
+  ret <- OpCl(stock_hist)*pos
+  # print(ret)
+  eq <- exp(cumsum(na.omit(ret)))
+  return(stock_hist)
 }
-
-performance_analysis <- function(stock_entry, cata = 'TW', month = 8, years = 0, draw = FALSE) {
+bband_analysis <- function(stock_entry, cata = 'TW', month = 8, years = 0, n = 22, sd = 1.5) {
+  print(stock_entry)
   stock_id = stock_entry
   stock_id <- paste(stock_id, sep = ".", cata)
-  print(stock_id)
-  
-  stock_hist <- get_stock_hist(stock_id, month, years)
+  diff_year = 0
+  diff_month = diff_year*12 + 8
+  ed_date = Sys.time()
+  # get stock history
+  st_date <- format((ed_date - as.difftime(diff_month*30, unit = "days")),"%Y-%m-%d")
+  stock_hist = na.omit(getSymbols(stock_id, auto.assign = FALSE, from = st_date))
+  if (nrow(stock_hist)<n)
+    return(c(stock_id, NA, NA))
   # print(stock_hist)
-
-  # calculate the performance by bbnad sd = 1.5
-  hold <- pick_strategy(stock_id, hist = stock_hist, draw = draw)
-
-  if (is.na(hold) || nrow(hold)<1) {
-    print("> ERROR:: hold is null return fail")
-    return(c(stock_id, NA))
-  }
-  # print("hold")
-  # print(hold)
-
-  buy <- Lag(hold)
-  # print("buy")
-  # print(buy)
-
-  # if (is.null(colnames(hold)))
-    # return(c(stock_id, NA))
-
+  #print(st_date)
+  #print(ed_date)
+  # calculate the performance of bbnad sd = 1.5
+  # pos <- bband_strat(stock_id, stock_hist)
+  hlc <- stock_hist[, -c(1,5,6)]
+  # print(hlc)
+  bb_data <- na.omit(BBands(na.omit(hlc), maType = 'SMA', n, sd))
+  # print(bb_data)
+  if (is.null(colnames(bb_data)))
+    return(c(stock_id, NA, NA))
+  buy <- na.omit(Lag(Cl(stock_hist))>Lag(bb_data$"up"))
   pos <- ifelse(buy, 1, 0)
-  # dbg usage 
-  if (0) {
-    # print(pos)
-    print("nrow(pos)")
-    print(nrow(pos))
-    print("nrow(stock_hist)")
-    print(nrow(stock_hist))
-  }
-  ret <- OpOp(stock_hist)*pos
-  # print(ret)
-
+  ret <- OpCl(stock_hist)*pos
   eq <- exp(cumsum(na.omit(ret)))
-  # print(eq)
-  return(c(stock_id, tail(eq, n=1)-1))
+  last_eq <- as.numeric(tail(eq, n=1))
+  # print(last_eq)
+
+  # calculate the bband last day (up-dn)/close
+  last_bb <- tail(bb_data, n=1)
+  last_hist <- tail(stock_hist, n=1)
+  bb_range <- last_bb$'up' - last_bb$'dn'
+  bumpy_range <- as.numeric(bb_range/Cl(stock_hist))
+  # print(bumpy_range)
+  result <- c(stock_id, bumpy_range, last_eq-1)
+  # print(result)
+  return(result)
 }
 
 
