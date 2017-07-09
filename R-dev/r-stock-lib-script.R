02bb7b1f04f5fb7418186c9acb45d126cdaf5260
WangKoko <yuchi.cc@gmail.com>
2017-07-09 23:21:23 +0800

    1) change pick criteria 2) add show plot for pick pos after pick

--- R-dev/r-stock-lib-script.R
+++ R-dev/r-stock-lib-script.R
@@ -17,7 +17,7 @@ get_stock_list <- function(cata = 'TW') {
 
 get_stock_hist <- function(stock_id, month = 8, years = 0) {
   diff_year = years
-  diff_month = diff_year*12 + 8
+  diff_month = diff_year*12 + month
   ed_date = Sys.time()
   # get stock history
   st_date <- format((ed_date - as.difftime(diff_month*30, unit = "days")),"%Y-%m-%d")
@@ -37,48 +37,33 @@ get_stock_hist <- function(stock_id, month = 8, years = 0) {
   return(stock_hist)
 }
 
-GET_VOLUME_POS_EXT <-function(sdata)
-{
-  if (20 > NROW(sdata))
-    return(NA)
-
-  vol5_pos <- runMean(sdata[,5], n = 5)
-  vol10_pos <- runMean(sdata[,5], n = 10)
-  vol20_pos <- runMean(sdata[,5], n = 20)
-
-  abs_vol_pos <- ifelse(sdata[,5] > vol10_pos, 1, 0)
-  vol5_pos <- vol5_pos > vol20_pos
-
-  return(abs_vol_pos & vol5_pos)
-}
-
-GET_MA_POS_EXT <- function(sdata, tgt = 60, ref = 120)
+GET_VOLUME_POS_EXT <-function(sdata, tgt = 5, ref = 10)
 {
   if (ref > NROW(sdata))
     return(NA)
 
-  tgt_ma <- runMean(sdata[,4], n = tgt)
-  ref_ma <- runMean(sdata[,4], n = ref)
+  tgt_pos <- runMean(Vo(sdata), n = tgt)
+  ref_pos <- runMean(Vo(sdata), n = ref)
+
+  abs_vol_pos <- ifelse(Vo(sdata) > tgt_pos, 1, 0)
+  vol_pos <- tgt_pos > ref_pos
+
+  return(vol_pos&abs_vol_pos)
+}
+
+GET_MA_POS_EXT <- function(sdata, tgt = 20, ref = 60)
+{
+  if (ref > NROW(sdata))
+    return(NA)
+
+  tgt_ma <- runMean(Cl(sdata), n = tgt)
+  ref_ma <- runMean(Cl(sdata), n = ref)
   ma_pos <- tgt_ma > ref_ma
   return(ma_pos)
 }
 
-# return hold position with array of TRUE FALSE corresponding to date
-pick_strategy <- function(stock_name, cata = 'TW'
-                           , hist = NULL, n = 22, sd = 1.5
-                           , month = 0, years = 1, draw = FALSE, pick = FALSE) {
-  if (is.null(hist)) {
-    if (!(cata == ''))
-      stock_name <- paste(stock_name, sep = ".", cata)
-    hist <- get_stock_hist(stock_name, month, years)
-    print(stock_name)
-    if (is.null(colnames(hist))||is.na(hist)) {
-      print("WARN: cannot download stock")
-      return(NA)
-    }
-    # print(hist)
-  }
-
+GET_BBand_POS_EXT <- function(hist, n = 22, sd = 1.5)
+{
   hlc <- hist[, -c(1,5,6)]
   # print(hlc)
   if (nrow(hist)<n) {
@@ -88,21 +73,200 @@ pick_strategy <- function(stock_name, cata = 'TW'
 
   bb_data <- na.omit(BBands(na.omit(hlc), maType = 'SMA', n, sd))
 
-  if (is.null(colnames(bb_data)))
+  bb_hold <- na.omit(Cl(hist)>bb_data$"up")
+  return(bb_hold)
+}
+
+# return test period for quantmod
+get_test_period <- function(month=4, years=0, from = Sys.time())
+{
+  test_month = years*12 + month
+  test_ed_date = as.Date(from)
+  # get stock history
+  test_st_date <- format((test_ed_date - as.difftime(test_month*30, unit = "days")),"%Y-%m")
+  test_ed_date <- format(test_ed_date, "%Y-%m")
+  test_period <- paste(test_st_date,sep = "::", test_ed_date)
+  return(test_period)
+}
+# show the pos in your strategy
+pos_plot <- function(stock_name, cata = 'TW', month = 4, years = 0, from = Sys.time(), FUNC = pick_strategy, ...)
+{
+  if (cata == '')
+    stock_idx <- stock_name
+  else
+    stock_idx <- paste(stock_name, sep = ".", cata)
+  hist <- get_stock_hist(stock_idx, month+4, years)
+  show_period <- get_test_period(month = month, from = from)
+  print(stock_idx)
+  print(show_period)
+  # print(hist[show_period])
+  # print(hist)
+  pos_hold <- FUNC(stock_name, hist = hist, month = month, years = years, ...)
+  # print(pos_hold)
+
+  chartSeries(hist[show_period], name = stock_idx, up.col = 'red', dn.col = 'green')
+  plot(addBBands(n = 22, sd = 1.5, maType = "SMA", draw = 'bands', on = 1), legend = NULL)
+  if (5 < nrow(hist)) {
+    ma_10<-runMean(Cl(hist),n=5)
+    plot(addTA(ma_10[show_period], on=1, col= 7, legend = NA))
+  }
+
+  if (10 < nrow(hist)) {
+    ma_10<-runMean(Cl(hist),n=10)
+    plot(addTA(ma_10[show_period], on=1, col= 6, legend = NA))
+  }
+
+  if (20 < nrow(hist)) {
+    ma_20<-runMean(Cl(hist),n=20)
+    plot(addTA(ma_20[show_period], on=1, col= 2, legend = NA))
+  }
+
+  if (60 < nrow(hist)) {
+    ma_60<-runMean(Cl(hist),n=60)
+    plot(addTA(ma_60[show_period],on=1,col= 3, legend = NULL))
+  }
+
+  if (120 < nrow(hist)) {
+    ma_120<-runMean(Cl(hist),n=120)
+    plot(addTA(ma_120[show_period],on=1,col=4, legend = NULL))
+  }
+  hold_price <- max(Cl(hist))*Lag(pos_hold)
+  # print(hold_price)
+  plot(addTA(hold_price[show_period],on=1,col=5))
+  invisible(readline(prompt="Press [enter] to continue"))
+}
+
+# with the bband and vol and sma the define when to enter
+pick_strategy2 <- function(stock_name, cata = 'TW'
+                          , hist = NULL, n = 22, sd = 1.5
+                          , month = 0, years = 1, pick = FALSE) {
+  if (is.null(hist)) {
+    if (!(cata == ''))
+      stock_name <- paste(stock_name, sep = ".", cata)
+    hist <- get_stock_hist(stock_name, month+4, years)
+    print(stock_name)
+    if (is.null(colnames(hist))||is.na(hist)) {
+      print("WARN: cannot download stock")
+      return(NA)
+    }
+    # print(hist)
+  }
+
+  test_period <- get_test_period(month = month, years = years)
+  hlc <- hist[, -c(1,5,6)]
+
+  # need to use 60 ma
+  if (nrow(hist)< 60) {
+    print("> ERROR:: not enough data to calculate SMA")
+    return(NA)
+  }
+
+  #1. b-band
+  bb_data <- na.omit(BBands(na.omit(hlc), maType = 'SMA', n, sd))
+  bb_hold <- na.omit(Cl(hist)>bb_data$"up")
+
+  # calculate the bband last day (up-dn)/mavg
+  bb_var <- (bb_data$'up' - bb_data$'dn')/bb_data$'mavg'
+  bb_var_limit <- ifelse(bb_var <= 0.12, 1, 0)
+
+  #2. calulate sma tangling within bbands
+  ma5 <- runMean(Cl(hist), n = 5)
+  ma10 <- runMean(Cl(hist), n = 10)
+  ma20 <- runMean(Cl(hist), n = 20)
+  ma60 <- runMean(Cl(hist), n = 60)
+
+  tangle5 <- (ma5<bb_data$'up') & (ma5>bb_data$'dn')
+  tangle10 <- (ma10<bb_data$'up') & (ma10>bb_data$'dn')
+  tangle20 <- (ma20<bb_data$'up') & (ma20>bb_data$'dn')
+  tangle60 <- (ma60<bb_data$'up') & (ma60>bb_data$'dn')
+  close <- (Cl(hist)<bb_data$'up') & (Cl(hist)>bb_data$'mavg')
+  bull <- ma20 > ma60
+  ma_hold <- tangle10[test_period] & tangle20[test_period] & bull[test_period] & tangle5[test_period] & close[test_period] & tangle60[test_period]
+  #3. volumn increase suddenly for several days
+  # vol_ma5 <- runMean(Vo(hist), n = 5)
+  vol_ma_ref <- runMean(Vo(hist), n = 10)
+  # vol_pos <- Vo(hist) > (vol_ma_ref)
+  vol_pos <- Vo(hist[test_period]) > 500000 # > 500 * 1000
+  vol_ma_pos <- Vo(hist[test_period]) > vol_ma_ref[test_period]
+  vol_hold <- vol_pos[test_period] & vol_ma_pos[test_period]
+  # print(Vo(hist))
+  # print(vol_hold)
+  if (is.null(vol_hold))
     return(NA)
 
+  # check hold
+  # hold <- bb_hold|(bb_var_limit[test_period] & ma_hold[test_period] & vol_hold[test_period])
+  hold <- bb_var_limit[test_period] & ma_hold[test_period] & vol_hold[test_period]
+  # hold <- vol_hold[test_period]
+  # print(hold) # debug for holding point
+  pos_hold <- ifelse(hold, 1, 0)
+
+  if(isTRUE(pick)) {
+    # holding position
+    last_2pos <- as.numeric(tail(pos_hold, n=2))
+    # volumn of last day for the stock
+    vol <- as.numeric(tail(Vo(hist), n=1))/1000
+    # calculate the profit
+    buy <- Lag(pos_hold)
+    buy <- ifelse(buy, 1, 0)
+    prof <- OpOp(hist)*buy
+    # print(ret)
+    eq <- exp(cumsum(na.omit(prof)))
+    prof <- tail(eq, n=1) - 1
+    # summarize
+    range <- as.numeric(tail(bb_var, n=1))
+    last_ma <- tail(ma_hold, n=1)
+    last_vo <- tail(vol_hold, n=1)
+    res <- data.frame(matrix(c(range, last_2pos[1], last_2pos[2], vol, prof, last_ma, last_vo), nrow=1, ncol=7))
+    colnames(res) <- c("var", "T-1", "T", "Vo", "Prof", "Last_ma", "Last_vo")
+    rownames(res) <- stock_name
+    # eq3[with(eq3, order(var)), ] # order with column var
+    return(res)
+  }
+  return(pos_hold)
+}
+# return hold position with array of TRUE FALSE corresponding to date
+pick_strategy <- function(stock_name, cata = 'TW'
+                           , hist = NULL, n = 22, sd = 1.5
+                           , month = 0, years = 1, pick = FALSE) {
+  if (is.null(hist)) {
+    if (!(cata == ''))
+      stock_name <- paste(stock_name, sep = ".", cata)
+    hist <- get_stock_hist(stock_name, month+4, years)
+    print(stock_name)
+    if (is.null(colnames(hist))||is.na(hist)) {
+      print("WARN: cannot download stock")
+      return(NA)
+    }
+    # print(hist)
+  }
+
+  test_period <- get_test_period(month = month, years = years)
   # print(bb_data)
   #1. b-band
+  hlc <- hist[, -c(1,5,6)]
+  # print(hlc)
+  if (nrow(hist)<n) {
+    print("> ERROR:: not enough data to calculate SMA")
+    return(NA)
+  }
+  bb_data <- na.omit(BBands(na.omit(hlc), maType = 'SMA', n, sd))
   bb_hold <- na.omit(Cl(hist)>bb_data$"up")
+
   #2. ma x > ma y
   ma_hold <- GET_MA_POS_EXT(hist, tgt = 20, ref = 60)
-  #3. volume - 5 ave. > 20 ave. buy & keep, otherwise, sell
-  vol_hold <- GET_VOLUME_POS_EXT(hist)
-
-  if (is.null(colnames(bb_hold))||is.null(ma_hold)||is.null(vol_hold))
+  if (is.null(ma_hold))
     return(NA)
 
-  hold <- bb_hold & ma_hold & vol_hold
+  #3. volume - 5 ave. > 20 ave. buy & keep, otherwise, sell
+  # vol_hold <- GET_VOLUME_POS_EXT(hist)
+  vol_hold <- rep(1, nrow(bb_hold))
+  if (is.null(vol_hold))
+    return(NA)
+
+  # check hold
+  # hold <- bb_hold & ma_hold & vol_hold
+  hold <- bb_hold[test_period] & ma_hold[test_period]
   # print(hold) # debug for holding point
   pos_hold <- ifelse(hold, 1, 0)
 
@@ -135,32 +299,6 @@ pick_strategy <- function(stock_name, cata = 'TW'
     rownames(res) <- stock_name
     # eq3[with(eq3, order(var)), ] # order with column var
     return(res)
-  } else if (isTRUE(draw)) {
-    show_month = 4
-    show_ed_date = Sys.time()
-    # get stock history
-    show_st_date <- format((show_ed_date - as.difftime(show_month*30, unit = "days")),"%Y-%m")
-    show_ed_date <- format(show_ed_date, "%Y-%m")
-    show_period <- paste(show_st_date,sep = "::", show_ed_date)
-    chartSeries(hist[show_period], name = stock_name, up.col = 'red', dn.col = 'green')
-    plot(addBBands(n = 22, sd = 1.5, maType = "SMA", draw = 'bands', on = 1), legend = NULL)
-    if (10 < nrow(hist)) {
-      ma_10<-runMean(hist[,4],n=10)
-      plot(addTA(ma_10[show_period], on=1, col="blue", legend = NA))
-    }
-    if (60 < nrow(hist)) {
-      ma_60<-runMean(hist[,4],n=60)
-      plot(addTA(ma_60[show_period],on=1,col="yellow", legend = NULL))
-    }
-    if (120 < nrow(hist)) {
-      ma_120<-runMean(hist[,4],n=120)
-      plot(addTA(ma_120[show_period],on=1,col=8, legend = NULL))
-    }
-    twii_hist <- get_stock_hist('^TWII', month, years)
-    plot(addTA(Cl(twii_hist[show_period]), on=NA, col="red", legend = NA))
-    hold_price <- max(Cl(hist))*Lag(pos_hold)
-    # print(hold_price)
-    plot(addTA(hold_price[show_period],on=1,col="grey"))
   }
   return(pos_hold)
 }
@@ -199,7 +337,7 @@ performance_analysis <- function(stock_entry, cata = 'TW', month = 8, years = 0,
     print("nrow(stock_hist)")
     print(nrow(stock_hist))
   }
-  ret <- OpOp(stock_hist)*pos
+  ret <- ClCl(stock_hist)*pos
   # print(ret)
 
   eq <- exp(cumsum(na.omit(ret)))
--- test-dev.R
+++ test-dev.R
@@ -11,8 +11,8 @@ if (isTRUE(do_pick)) {
   print(c("> start pick", today))
   # pick <- do.call(rbind, lapply(head(tw_list$No, n=28), FUN=pick_strategy, cata = 'TW', pick = TRUE))
   # pick_otc <- do.call(rbind, lapply(head(two_list$No, n=10), FUN=pick_strategy, cata = 'TWO', pick = TRUE))
-  pick <- do.call(rbind, lapply(tw_list$No, FUN=pick_strategy, cata = 'TW', pick = TRUE))
-  pick_otc <- do.call(rbind, lapply(two_list$No, FUN=pick_strategy, cata = 'TWO', pick = TRUE))
+  pick <- do.call(rbind, lapply(tw_list$No, FUN=pick_strategy2, cata = 'TW', pick = TRUE))
+  pick_otc <- do.call(rbind, lapply(two_list$No, FUN=pick_strategy2, cata = 'TWO', pick = TRUE))
 
   # fix NA value
   pick <- na.omit(pick)
@@ -20,16 +20,11 @@ if (isTRUE(do_pick)) {
   pick_all <- rbind(pick, pick_otc)
 
   # Get those stock is NOT held yesterday but DO held today
-  pick_today <- pick_all[pick_all$'T-1'==0 & pick_all$'T'==1,]
-  pick_today_2 <- pick_all[pick_all$'T'==1,]
-  # Get those stock is held MORE than TWO days
-  pick_than_2days  <- pick_all[pick_all$'T-1'==1 & pick_all$'T'==1,]
+  pick_today <- pick_all[pick_all$'T'==1,]
   # order with column var
   pick_all <- pick_all[with(pick_all, order(var)), ]
   pick_today <- pick_today[with(pick_today, order(var)), ]
-  pick_today_vol <- pick_today[with(pick_today, order(Vo, decreasing = TRUE)), ]
-  pick_than_2days <- pick_than_2days[with(pick_than_2days, order(var)), ]
-  pick_today_2 <- pick_today_2[with(pick_today_2, order(var)), ]
+  pick_all_prof <- pick_all[with(pick_all, order(Prof, decreasing = TRUE)), ]
   # write to file
   f_name <- paste("pick-", sep = "", today)
   f_name <- paste(f_name, sep = "", ".csv")
@@ -37,9 +32,10 @@ if (isTRUE(do_pick)) {
 
   f_name <- paste("pick-today", sep = "", today)
   f_name <- paste(f_name, sep = "", ".csv")
-  write.csv(pick_today_2, file = f_name)
+  write.csv(pick_today, file = f_name)
   print("> Pick end!")
   # pick_all[pick_all$Prof < 0,]
+  prof_mean <- mean(as.numeric(pick_all$'Prof'))
   win_rate <- sum(ifelse(pick_all$'Prof'>=0, 1, 0))/nrow(pick_all)
 }
 
@@ -61,5 +57,9 @@ if (isTRUE(do_perf_check)) {
 }
 
 # see single stock analysis
-source("R-dev/r-stock-lib-script.R")
-pick_strategy('1216', cata = 'TW', draw = TRUE)
+# source("R-dev/r-stock-lib-script.R")
+# pick_strategy('1216', cata = 'TW', draw = TRUE)
+# pos_plot('2834', cata = 'TW', month = 4, years = 0, from = Sys.time(), FUNC = pick_strategy2)
+if (isTRUE(do_pick)) {
+  plot_show <- lapply(rownames(pick_today), FUN=pos_plot, cata = '', FUNC = pick_strategy2)
+}
